595bc3d309c20e3c142e8c18479ed7bbf9173fb6
Finnik <73883383+golkity@users.noreply.github.com>
2024-05-01 17:30:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
+import './App.css';
+import  TodoWrapper  from './Components/TodoWrapper.jsx';
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+}
 
+export default App;
